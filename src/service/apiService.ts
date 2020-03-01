e19d323fb5c93ae80de4e25924e51b41322609dd
Anton Ovcharenko <majestiko@ukr.net>
2020-03-01 11:13:31 +0200

    debug api

--- src/service/apiService.ts
+++ src/service/apiService.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 import {BASE_URL} from "../config/config";
-import {Simulate} from "react-dom/test-utils";
-import error = Simulate.error;
+
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.post['x-access-token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTE5YzIyM2E0MTk5YzAwMjI3NTI2OGEiLCJpYXQiOjE1Nzk2ODc4OTl9.M5q83O_nP6B8SbfNKOs3CaQTu4JaQcbr_MgDLSgqnTU'
 
